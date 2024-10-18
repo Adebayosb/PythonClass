cef5b34a54c0a6bcebb2db56be0a852231816c36
Adebayosb <adebayo.0709@gmail.com>
2024-10-18 22:52:09 +0100

    assignment

---
+++ Chapter 2/Exponentiation.py
@@ -0,0 +1,20 @@
+number1 = 10 ** 100
+number2 = 2 ** 10000
+number3 = 3 ** 1000
+
+print("10 ** 100:", number1)
+print("2 ** 10000:", number2)
+print("3 ** 1000:", number3)
+
+
+
+
+
+
+
+
+
+
+
+
+
