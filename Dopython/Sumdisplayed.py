fa061d757d1c9dc9888e3072980d93a81147a344
Adebayosb <adebayo.0709@gmail.com>
2024-10-21 00:17:21 +0100

    assignment

---
+++ Dopython/Sumdisplayed.py
@@ -0,0 +1,9 @@
+while True:
+	number_1 = int(input("Enter first number: "))
+	number_2 = int(input("Enter second number: "))
+	sum = number_1 + number_2
+	print(sum)
+	word = input("press n to stop or any key to continue: ")
+	if word == "n":
+		break
+	
---
+++ Dopython/powerofanother.py
@@ -0,0 +1,10 @@
+firstNumber = int(input("Enter firstnumber: "))
+secondNumber = int(input("Enter secondNumber: "))
+
+some = 1
+c = 0
+while c<secondNumber:
+	some *= firstNumber
+	c +=1
+print(some)
+
